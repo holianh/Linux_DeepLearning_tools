6a5d38d9e33f0b5bb0dd24fa8f05cdf050a28333
Tuấn Anh <tuananhvxl@gmail.com>
2018-01-22 10:43:04 +0800

    Update python_funcs_codes.py

--- python_funcs_codes.py
+++ python_funcs_codes.py
@@ -3,6 +3,83 @@ Nguyen Tuan Anh, 2018-01-19
 usful python functions
 --------------------------
 '''
+
+########################################################################
+# Plot history and accuray to PDF --------------------------------------
+########################################################################
+
+import matplotlib                                    ###############
+matplotlib.use('agg')
+import matplotlib.pyplot as plt 
+from matplotlib.backends.backend_pdf import PdfPages
+
+def hist(history,
+         strNeedPlot=('loss','acc'),#,'loss'),#,'acc','loss','acc'),
+         Labels=(['Training Loss'    ,'Epochs','Loss value'],
+                 ['Training Accuracy','Epochs','Accuracy value'],
+                 ['Label 3','X label3','Ylabel3'],
+                 ['Label 4','X label4','Ylabel4'],
+                 ['Label 5','X label5','Ylabel5'],
+                 ['Label 6','X label6','Ylabel6'],
+                 ),
+          NoColumns=1,
+          nb_plots_per_page = 3,               # Total number in one page to print.
+          fdfName='history_training.pdf'
+         ):
+    # Plot history of training in keras to pdf
+    # history=model.fit(...) in keras
+    # history={'loss':[1,2,3,...n],'acc':[1,2,3,...,m]}
+    # strNeedPlot=('loss','acc','loss','acc','loss')
+
+    pdf_pages = PdfPages(fdfName)
+    nb_plots = len(strNeedPlot)         # Number of images
+    if(nb_plots_per_page>nb_plots):nb_plots_per_page=nb_plots;
+    nb_pages = int(np.ceil(nb_plots / float(nb_plots_per_page)))#No of Pages
+    grid_size = (nb_plots_per_page, NoColumns)  # Number of columns
+    PaperSize=A4=(8.27, 11.69)          # paper size in inch
+    i=0
+    for sType in strNeedPlot:
+        # Create a figure instance (ie. a new page) if needed
+        if i % nb_plots_per_page == 0: fig = plt.figure(figsize=PaperSize, dpi=100) 
+        # Plot stuffs !
+        ax=plt.subplot2grid(grid_size, (i % nb_plots_per_page, 0))
+        plt.subplots_adjust(wspace=0, hspace=.5)
+        plt.subplots_adjust(left=0.2)
+        plt.subplots_adjust(top=0.88)
+        plt.plot(history.history[sType])
+        
+        ax.set_title(Labels[i][0])
+        plt.xlabel(Labels[i][1], fontsize=14)
+        plt.ylabel(Labels[i][2], fontsize=14)
+        
+        # Close the page if needed
+        if (i + 1) % nb_plots_per_page == 0 or (i + 1) == nb_plots:
+            #plt.tight_layout()
+            pdf_pages.savefig(fig)
+        i+=1
+        ''' #examples
+        fig = plt.figure()
+        ax1 = fig.add_subplot(221)
+        ax2 = fig.add_subplot(222)
+        ax3 = fig.add_subplot(223)
+        ax4 = fig.add_subplot(224)
+        ax1.title.set_text('First Plot')
+        ax2.title.set_text('Second Plot')
+        ax3.title.set_text('Third Plot')
+        ax4.title.set_text('Fourth Plot')
+        plt.show()
+        '''
+    # Write the PDF document to the disk
+    pdf_pages.close()
+    print(fdfName + " - already save successful!")
+    print("to view, please see at:" + os.getcwd())
+    # How to use:
+    #   history = model.fit(x_data, y_data, epochs=5000,verbose=1)
+    #   hist(history)
+
+# hist(history)    
+# exit()
+
 ########################################################################
 # dirs / files --------------------------------------------------
 ########################################################################
