c9a91ec1ce035a9c98c5f56ff3c2e94b57aa9c33
Tuấn Anh <tuananhvxl@gmail.com>
2018-01-25 09:02:59 +0800

    Update ubuntu_commands.sh

--- ubuntu_commands.sh
+++ ubuntu_commands.sh
@@ -11,7 +11,11 @@ nvidia-smi --format=csv --query-gpu=index,name,temperature.gpu,fan.speed,pstate,
 
 Output: 0, GeForce GTX 1080 Ti, 78, 46 %, P2, 178.71 W, 1797 MHz
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
+Find files or folders in Ubuntu:
+- find -name "\*ASR\*" : find all file/dir name include ASR
+- find -typy d -name "\*ASR\*" : find all *dir* name include ASR
+- find -type f -name "*libcudart*"   : find all files libcudart.so.* in current folder
+- find -type f -name "*libcudnn.so*"   : find all files libcudnn.so.*
 
 
 
