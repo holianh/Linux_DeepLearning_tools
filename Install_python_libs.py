1b95faf60aff0070ccbb915579ae60f7b588376f
Tuấn Anh <tuananhvxl@gmail.com>
2018-03-20 17:21:22 +0800

    Create Install_python_libs.py

---
+++ Install_python_libs.py
@@ -0,0 +1,43 @@
+from concurrent.futures import ThreadPoolExecutor
+from Levenshtein import distance
+from matplotlib.backends.backend_pdf import PdfPages
+from os.path import basename
+from string import whitespace
+import datetime as dt
+import io
+import keras
+import lasagne
+import librosa
+import logging
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np;
+import os;
+import pandas as pd
+import pickle;
+import random
+import soundfile
+import sys;
+import wave
+
+
+
+
+
+
+######################################################################
+######################################################################
+pip install pypinyin
+pip install pipreqs
+pip install lasagne
+pip install librosa --upgrade
+
+
+
+######################################################################
+######################################################################
+## Conda:
+conda install -c anaconda anaconda-navigator
+
+
+
