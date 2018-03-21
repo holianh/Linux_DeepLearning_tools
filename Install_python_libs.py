5cfcc8645f2e4c50c7e7230d39b6253cec964b23
Tuấn Anh <tuananhvxl@gmail.com>
2018-03-21 12:06:03 +0800

    Update Install_python_libs.py

--- Install_python_libs.py
+++ Install_python_libs.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from Levenshtein import distance
 from matplotlib.backends.backend_pdf import PdfPages
-from os.path import basename
+from os.path import  join,join as jj,  exists,exists as tt,  basename,basename as bname
 from string import whitespace
 import datetime as dt
 import io
@@ -16,9 +16,13 @@ import os;
 import pandas as pd
 import pickle;
 import random
+import re
 import soundfile
 import sys;
 import wave
+import shutil
+import pypinyin
+
 
 
 
@@ -26,18 +30,40 @@ import wave
 
 
 ######################################################################
+Installing via PIP
 ######################################################################
 pip install pypinyin
 pip install pipreqs
 pip install lasagne
 pip install librosa --upgrade
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+sudo apt-get install libsamplerate0 libsamplerate0-dev
+pip install scikits.samplerate
+pip install info
 
 
 
 ######################################################################
+Installing via Anaconda
 ######################################################################
-## Conda:
+
 conda install -c anaconda anaconda-navigator
+conda install scikit-learn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
