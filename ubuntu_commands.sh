1acbcdf5de2892cab89c5052ecc810c37630633f
Tuấn Anh <tuananhvxl@gmail.com>
2018-09-06 22:46:36 +0800

    Update ubuntu_commands.sh

--- ubuntu_commands.sh
+++ ubuntu_commands.sh
@@ -347,6 +347,24 @@ https://stackoverflow.com/questions/35254852/how-to-change-the-jupyter-start-up-
 
 
 
+##############################################################
+Anaconda Jupyter Notebook: Run Remote Notebook on server from local:
+##############################################################
+1. connect Xshell/ ssh to server.
+2. Run On server: (example: 1080ti server)
+    conda info --envs
+    source activate tf
+  ipython notebook --no-browser --ip=* --port=1234
+(port can be change)
+result:
+http://(ubu or 127.0.0.1):1234/?token=113fbcd684007d6a72f8839c94637951d613b136a39b040c
+
+3. Run on local:
+- copy that address, paste to local browser
+- change IP address to real IP: http://10.1.53.1:1234/?token=113fbcd684007d6a72f8839c94637951d613b136a39b040c
+- Enjoy!
+
+
 
 
 
