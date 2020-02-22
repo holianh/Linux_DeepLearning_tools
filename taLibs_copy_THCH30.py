e4610c6262431067825e21503e98ef5d3d97ffab
Tuấn Anh <tuananhvxl@gmail.com>
2020-02-22 23:33:20 +0700

    Update taLibs_copy_THCH30.py

--- taLibs_copy_THCH30.py
+++ taLibs_copy_THCH30.py
@@ -1,3 +1,28 @@
+# %%writefile Copy_THCH30_From_GoogleDrive_2_Local.py
+Colab=True
+import shutil
+import os
+os.system('apt install pv')
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# if not os.path.exists("/content/DeepSpeechRecognition"):
+#   !git clone https://github.com/audier/DeepSpeechRecognition.git
+#   !git clone https://github.com/zw76859420/ASR_Syllable.git
+
+#-------------------------------------------------------------------------------
+import zipfile
+from tqdm import tqdm
+def taLibs_Unzip_progress(src='thch30_train_dev_test.zip', target_path='.'):
+  with zipfile.ZipFile(src) as zf:
+      for member in tqdm(zf.infolist(), desc='Extracting '):
+          try:
+              zf.extract(member, target_path)
+          except zipfile.error as e:
+              pass
+
+#-------------------------------------------------------------------------------
 import glob, shutil
 import os
 from os.path import join, exists
@@ -7,33 +32,32 @@ def startCopy_models(source,dest):
     for f in files: os.remove(f)            
     for file in glob.glob(os.path.join(source,"*.*")):
         shutil.copy2(file,dest)
+
+#-------------------------------------------------------------------------------
 def fn_copy_data_thchs30():
-    if not os.path.exists("/content/data_thchs30"):
-        # !cp "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  "thch30_train_dev_test.zip" 
-        print("Copy thch30_train_dev_test.zip to colab:")
-        fnsrc="/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"
-        os.system('rsync --info=progress2 "{}"  "./thch30_train_dev_test.zip" '.format(fnsrc))
-        
-        print('finshed copy!\nUnzip...:')
-        #!unzip -qq thch30_train_dev_test.zip
-        os.system('unzip -o thch30_train_dev_test.zip -d /. | pv -l >/dev/null')
-        if os.path.exists('content'):
-            os.system('mv content/* .')
-            #os.system('rm -d content')
-            shutil.rmtree('content')         
-        os.remove('rm thch30_train_dev_test.zip')
-        
+  if not os.path.exists ('/content/drive'):
+    from google.colab import drive
+    drive.mount('/content/drive')
+  if not os.path.exists("/content/data_thchs30"):
+      print("start copy thch30_train_dev_test to local...")      
+      ! rsync --progress "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  .     
+      taLibs_Unzip_progress(src='thch30_train_dev_test.zip', target_path='.')
+      for file in glob.glob("./content/*"): shutil.move(file,dest)
+      os.remove('thch30_train_dev_test.zip')
+  print("Done copy data")
+#-------------------------------------------------------------------------------
 if __name__ == '__main__':
     print("run taLibs_copy_THCH30.py file")
     fn_copy_data_thchs30()
-'''
-How to use?
 
-import os
-os.system('rm -rf taLibs_copy_THCH30.*')  
-!wget -q https://github.com/holianh/Linux_DeepLearning_tools/raw/master/taLibs_copy_THCH30.py  
-!ls -la -h taLibs*
-import taLibs_copy_THCH30 as datathch30
-# datathch30.fn_copy_data_thchs30()
+'''
+    How to use?
+
+    import os
+    os.system('rm -rf taLibs_copy_THCH30.*')  
+    !wget -q https://github.com/holianh/Linux_DeepLearning_tools/raw/master/taLibs_copy_THCH30.py  
+    !ls -la -h taLibs*
+    import taLibs_copy_THCH30 as datathch30
+    # datathch30.fn_copy_data_thchs30()
 
 '''
