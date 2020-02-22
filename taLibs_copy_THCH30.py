33b7ad57912f4c935c2d185e6449998e5a98eb5d
Tuấn Anh <tuananhvxl@gmail.com>
2020-02-22 10:56:27 +0700

    Update taLibs_copy_THCH30.py

--- taLibs_copy_THCH30.py
+++ taLibs_copy_THCH30.py
@@ -7,14 +7,19 @@ def startCopy_models(source,dest):
     for f in files: os.remove(f)            
     for file in glob.glob(os.path.join(source,"*.*")):
         shutil.copy2(file,dest)
-
-if not os.path.exists("/content/data_thchs30"):
-    # !cp "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  "thch30_train_dev_test.zip" 
-    print("Copy thch30_train_dev_test.zip to colab:")
-    !rsync --info=progress2 "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  "./thch30_train_dev_test.zip" 
-    print('finshed copy!\Unzip...:')
-    #!unzip -qq thch30_train_dev_test.zip
-    !unzip -o thch30_train_dev_test.zip -d /. | pv -l >/dev/null
-    !mv content/* .
-    !rm -d content
-    !rm thch30_train_dev_test.zip
+def fn_copy_data_thchs30():
+    if not os.path.exists("/content/data_thchs30"):
+        # !cp "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  "thch30_train_dev_test.zip" 
+        print("Copy thch30_train_dev_test.zip to colab:")
+        os.system('rsync --info=progress2 "/content/drive/My Drive/Deeplearning/thch30_train_dev_test.zip"  "./thch30_train_dev_test.zip" ')
+        print('finshed copy!\Unzip...:')
+        #!unzip -qq thch30_train_dev_test.zip
+        os.system('unzip -o thch30_train_dev_test.zip -d /. | pv -l >/dev/null')
+        os.system('mv content/* .')
+        #os.system('rm -d content')
+        shutil.rmtree('content')         
+        os.remove('rm thch30_train_dev_test.zip')
+        
+if __name__ == '__main__':
+    print("run taLibs_copy_THCH30.py file")
+    fn_copy_data_thchs30()
