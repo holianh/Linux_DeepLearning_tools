c99cc416f0b1882f7bf34ab34e15ba4be2f3ad95
Tuấn Anh <tuananhvxl@gmail.com>
2020-02-25 23:09:37 +0700

    Update taLibs_ASR_utils.py

--- taLibs_ASR_utils.py
+++ taLibs_ASR_utils.py
@@ -1,15 +1,23 @@
 # ------------------------------- New ------------------------------------------------
-# thisModel_Name='ASR-CNN+CTC_tutorial_train_test'
-import glob, shutil
-import os
+# thisModel_Name='ASR-CN_CTC_tutorial_train_test'
+import glob, shutil, os 
 from os.path import join, exists
-if not exists('results'):os.makedirs('results')
+
 try:
   import gpustat
 except:
   os.system("pip install gpustat")
   import gpustat
+
+SavingDir = "results/{}_results".format(thisModel_Name)
+if exists('/content/sample_data/anscombe.json'):
+    Colab=True
+    SavingDir = "drive/My Drive/Deeplearning/ASR---results/{}_results".format(thisModel_Name)
+    
+if not exists('results'):os.makedirs('results')
+  
 def startCopy_models(source,dest):
+    if source==dest: return
     if not exists(dest): os.makedirs(dest)
     files = glob.glob(os.path.join(dest,"*"))
     for f in files: os.remove(f)            
@@ -17,14 +25,19 @@ def startCopy_models(source,dest):
         shutil.copy2(file,dest)
 
 def load_model(self, filename='results/{}'.format(thisModel_Name) ):
-    if not exists (filename + '.model') :
-        startCopy_models(dest = "results", source = "/content/drive/My Drive/Deeplearning/ASR---results/{}_results".format(thisModel_Name)) 
-        print("Copy result inner load_model from gdrive to local, done!")
-    self.ctc_model.load_weights(filename + '.model')
+    if Colab:
+        if not exists (filename + '.model') :
+            startCopy_models(dest = "results", source = SavingDir) 
+            print("Copy result inner load_model from gdrive to local, done!")
+    if exists(filename + '.model'):     self.ctc_model.load_weights(filename + '.model')
     if exists(filename + '.model.base'):self.model.load_weights(filename + '.model.base')
-    if exists(filename + '.loss.npy'): self.loss=np.load(filename + '.loss.npy')
+    if exists(filename + '.loss.npy'):  self.loss=np.load(filename + '.loss.npy')
 
-def save_model(self , filename='results/{}'.format(thisModel_Name) , comments='',loss=None):
+def save_model(self , filename='results/{}'.format(thisModel_Name) , comments='',loss=None, save_best_only=True):
+    if save_best_only:
+        if self.minloss < np.mean(loss[-10:]):
+            return
+    self.minloss = np.mean(loss[-10:])
     mydir="/".join(filename.split('/')[:-1])        
     if not exists(mydir): os.makedirs(mydir)        
     fnName=filename #+ comments
@@ -33,15 +46,13 @@ def save_model(self , filename='results/{}'.format(thisModel_Name) , comments=''
     f = open(fnName + '_steps.txt' , 'a+')           
     f.write(filename + comments+'\n')
     f.close()
-    if loss:
-        np.save(fnName + '.loss.npy',loss)
-    dest="/content/drive/My Drive/Deeplearning/ASR---results/{}_results".format(thisModel_Name)
-    startCopy_models(source = "results", dest=dest) 
-    print('File saved to:',dest)
+    if loss: np.save(fnName + '.loss.npy',loss)
+    startCopy_models(source = "results", dest=SavingDir) 
+    print('File saved to:',SavingDir)
     print('-'*60) 
 
-def plotLoss(loss,thisModel_Name, bottom=0):
-    clear_output()
+def plotLoss(loss,thisModel_Name, bottom=0, save=False):
+    if Colab:clear_output()
     loss_mean=np.zeros(len(loss))
     loss_mean[0]=loss[0]
     for k in range(1,len(loss)): loss_mean[k] = 0.95*loss_mean[k-1] + 0.05*loss[k]
@@ -56,7 +67,7 @@ def plotLoss(loss,thisModel_Name, bottom=0):
     plt.minorticks_on()
     plt.grid(which='major', linestyle='--', linewidth='0.3', color='black')
     plt.grid(which='minor', linestyle=':', linewidth='0.1', color='black')
-    plt.savefig('results/{}.svg'.format(thisModel_Name))
+    if save:plt.savefig('results/{}.svg'.format(thisModel_Name))
     plt.show()
 
 # -----------------------------------------------------------------
@@ -66,6 +77,17 @@ def plotLoss(loss,thisModel_Name, bottom=0):
 # test_loss=am.ctc_model.evaluate(x=batch,steps=1)
 # print('Test_loss After Load weight=',test_loss)
 # print("="*60)
-def modulereload(modulename):
-    import importlib
-    importlib.reload(modulename)
+
+# use:-------------------------------------------------------------
+# import urllib.request  
+# import importlib
+
+# url="https://github.com/holianh/Linux_DeepLearning_tools/raw/master/taLibs_ASR_utils.py"
+
+# filename, headers = urllib.request.urlretrieve(url,filename=url.split('/')[-1]) 
+# import taLibs_ASR_utils as ASRutil
+# importlib.reload(ASRutil)
+
+# ASRutil.load_model()
+# ASRutil.save_model()
+# ASRutil.plotLoss()
